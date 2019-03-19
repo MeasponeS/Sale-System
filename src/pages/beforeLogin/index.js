b67e80357ac8c8e6c631e30d5cb6550d3a628b1f
MeasponeS <42404280+MeasponeS@users.noreply.github.com>
2019-03-19 17:40:04 +0800

    Update index.js

--- src/pages/beforeLogin/index.js
+++ src/pages/beforeLogin/index.js
@@ -8,7 +8,7 @@ import {wxSignature} from "../../api/wechat";
     if(!window.URLPARAMS.hasOwnProperty('code')){
         wxSignature({url:window.location.href.split('?')[0]}).then(r=>{
             window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid="  +
-                r.signature.appId  + "&redirect_uri=" + encodeURIComponent(window.location.href.split('?')[0])  +   "&response_type=code&scope=snsapi_userinfo&state=jkhh#wechat_redirect"
+                r.signature.appId  + "&redirect_uri=" + encodeURIComponent('https://hsj.hulian120.com/beforeLogin.html')  +   "&response_type=code&scope=snsapi_userinfo&state=jkhh#wechat_redirect"
 
         }).catch(_=>{})
     }
