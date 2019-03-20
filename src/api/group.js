e75e0d579115652fd83938ddebbe5535eaf1e37c
MeasponeS <42404280+MeasponeS@users.noreply.github.com>
2019-03-20 16:04:52 +0800

    Update group.js

--- src/api/group.js
+++ src/api/group.js
@@ -5,7 +5,7 @@ import request from '../utils/request.js'
 // 分享链接开团
 export function crtGroupOpen(params) {
     return request({
-        url: '/sale/api/crtGroupOpen',
+        url: '/sale/api/group/crtGroupOpen',
         method: 'post',
         data: params
     })
