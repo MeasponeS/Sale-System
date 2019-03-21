993b39d1fcb44dce50dd4a62a9bf7d98f867be9e
MeasponeS <42404280+MeasponeS@users.noreply.github.com>
2019-03-21 14:48:58 +0800

    15

--- src/pages/beforeLogin/Index.vue
+++ src/pages/beforeLogin/Index.vue
@@ -15,6 +15,7 @@
     import {Button as vantButton,Toast} from 'vant';
     import {Icon as vantIcon} from 'vant';
     import {sendSmsCode} from '../../api/recommender'
+    import Config from '../../config/app'
 
     export default {
         name: 'app',
@@ -30,8 +31,8 @@
                     Toast('请输入正确的手机号');
                     return;
                 }
-                sendSmsCode({mobile:this.mobile,activityId: window.URLPARAMS.actId}).then(r=>{
-                    window.location.href = './login.html?mobile=' + this.mobile + '&code=' + window.URLPARAMS.code + '&actId=' + window.URLPARAMS.actId;
+                sendSmsCode({mobile:this.mobile,activityId:Config.activityId}).then(r=>{
+                    window.location.href = './login.html?mobile=' + this.mobile + '&code=' + window.URLPARAMS.code + '&actId=' + Config.activityId;
                 }).catch(_=>{});
             }
         },
