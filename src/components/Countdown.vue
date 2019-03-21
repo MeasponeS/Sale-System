6873ca5ee9e27d7df6b1025ccfae50050694e709
MeasponeS <42404280+MeasponeS@users.noreply.github.com>
2019-03-21 21:49:59 +0800

    37

--- src/components/Countdown.vue
+++ src/components/Countdown.vue
@@ -19,31 +19,46 @@
             }
         },
         mounted() {
-            console.log(this.second);
-            if (this.second == 0) return;
-            let initSecond = this.second;
-            let that = this;
-            window.Countdown = setInterval(_ => {
-                if (initSecond <= 0) {
-                    this.$emit('end');
-                    clearInterval(window.Countdown);
-                    return
-                }
-                initSecond--;
-                this.$emit('toggle');
-
-                let hour = Math.floor(initSecond / 3600);
-                let minute = Math.floor(initSecond % 3600 / 60);
-                let second = Math.floor(initSecond % 60);
-                console.log(that.h, that.m, that.s);
-                that.h = (hour < 10 ? '0' + hour : hour);
-
-                that.m = (minute < 10 ? '0' + minute : minute);
-
-                that.s = (second < 10 ? '0' + second : second);
-            }, 1000)
-
+            this.initFn()
         },
+        watch:{
+              'second':{
+                  deep:true,
+                  handler:function (newVal,oldVal) {
+                      if(newVal > 0){
+                          console.log(newVal);
+                          this.initFn()
+                      }
+                  }
+              }
+        },
+        methods:{
+            initFn(){
+                if (this.second == 0) return;
+                let initSecond = this.second;
+                let that = this;
+                window.Countdown = setInterval(_ => {
+                    if (initSecond <= 0) {
+                        this.$emit('end');
+                        clearInterval(window.Countdown);
+                        return
+                    }
+                    initSecond--;
+
+
+                    let hour = Math.floor(initSecond / 3600);
+                    let minute = Math.floor(initSecond % 3600 / 60);
+                    let second = Math.floor(initSecond % 60);
+                    console.log(that.h, that.m, that.s);
+                    that.h = (hour < 10 ? '0' + hour : hour);
+
+                    that.m = (minute < 10 ? '0' + minute : minute);
+
+                    that.s = (second < 10 ? '0' + second : second);
+                }, 1000)
+
+            }
+        }
 
 
     }
