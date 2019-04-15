84f0c8103fc3a7ec773a87ad5767124608ad3961
dengchangjiang <dengchangjiang@hulian120.com>
2019-04-15 20:15:05 +0800

    测试图片

--- src/config/app.js
+++ src/config/app.js
@@ -1,11 +1,10 @@
-const devApiUrl = 'http://192.168.48.173:9992'; // 翔哥
+
+ const devApiUrl = 'https://testsale.hulian120.com'; // 翔哥
 // const devApiUrl = 'http://192.168.48.133:8081'; // 峰峰
 
 //正式环境变量,注意修改
-const proApiUrl = 'https://pay.hulian120.com';
-
-
-
+const proApiUrl = 'https://testsale.hulian120.com';
+//https://pay.hulian120.com
 const nodeDevEnv = process.env.NODE_ENV=='development' ? true : false;
 export default {
     apiUrl : nodeDevEnv ? devApiUrl : proApiUrl,
@@ -17,10 +16,11 @@ export default {
     countDown:60,//短信验证码倒计时
     tokenKey:'ACTIVITY_ACCESS_TOKEN',
     storageUserKey:'ACTIVITY_USER_STORAGE',
-    wxUrl:'https://wxauth.hulian120.com/open/getPayCodeFor',    // redirect_uri
-    shareUrl:'https://hsj.hulian120.com/pay/',
+    wxUrl:'https://wxauth.hulian120.com/open/getTestCodeFor',    // redirect_uri  https://wxauth.hulian120.com/open/getPayCodeFor
+    shareUrl:'https://hsj.hulian120.com/test/',
     requestRetry:4,
-    serverUrl:'https://pay.hulian120.com/sale/api/wx/login',
+    serverUrl:'https://testsale.hulian120.com/test/api/wx/login',
     //staticUrl:'https://hsj.hulian120.com',
     requestRetryDelay:800,
 }
+// https://hsj.hulian120.com/pay/
